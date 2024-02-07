b11bec8a0c5f18514cf0945a8fdbe806ae894240
CodeMode <99124573+CodeModeYT@users.noreply.github.com>
2024-02-07 13:03:34 +0100

    added first version of Task creation and listing

---
+++ src/lib/store.ts
@@ -0,0 +1,3 @@
+import { writable } from 'svelte/store'
+
+// const todos = writable<string[]>( localStorage.getItem('todo'))
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { writable } from 'svelte/store';
+
+	let valueC = '';
+
+	let tasks = writable<string[]>([]);
+
+	function addTask() {
+		const input = (document.getElementById('taskname') as HTMLInputElement).value.trim();
+		if (input !== '') {
+			tasks.update((currentTasks: any) => [...currentTasks, input]);
+			(document.getElementById('taskname') as HTMLInputElement).value = '';
+			isTasksEmpty();
+			let string = JSON.stringify($tasks);
+			localStorage.setItem('todos', string);
+		}
+	}
+
+	function handleEnterKey(event: KeyboardEvent) {
+		if (event.key === 'Enter') {
+			addTask();
+		}
+	}
+
+	function isTasksEmpty() {
+		let tasksArray = $tasks;
+		if (tasksArray.length !== 0) {
+			(document.getElementById('task-placeholder') as HTMLInputElement).style.display = 'none';
+		}
+	}
+</script>
+
+<input
+	type="text"
+	id="taskname"
+	placeholder="Enter the name of the task..."
+	maxlength="100"
+	on:keydown={handleEnterKey}
+/>
+<button id="input_button" on:click={addTask}>Add task!</button>
+<p id="task-placeholder">Start writing your first tasks...</p>
+<div class="task">
+	{#each $tasks as task, i}
+		<p>{task}</p>
+	{/each}
+</div>
